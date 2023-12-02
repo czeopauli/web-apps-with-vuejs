f7a7c41fe8bb870d63f9b0ab4ed674c7d48e725e
czeopauli <145656763+czeopauli@users.noreply.github.com>
2023-12-02 21:40:05 +0200

    Add files via upload

---
+++ 03-attribute-binding/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Vue.createApp({
+  data() {
+    return {
+      headline: "Attribute Binding is awesome",
+      imageAttrs: {
+        src: "https://picsum.photos/id/237/200/300",
+        alt: "A cute dog",
+      },
+    };
+  },
+}).mount("#app");
